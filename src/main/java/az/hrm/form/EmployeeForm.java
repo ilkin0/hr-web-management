e700c6cf1887cea32c98c7ee3262272a6428f942
Ilkin <mehdiyevilkin01@gmail.com>
2020-05-11 14:21:13 +0400

    Addin Bean Validation to project

---
+++ src/main/java/az/hrm/form/EmployeeForm.java
@@ -0,0 +1,15 @@
+package az.hrm.form.employeeForm;
+
+import java.math.BigDecimal;
+import java.sql.Date;
+
+public class EmployeeForm {
+    private long id = 0L;
+    private String firstName = "";
+    private String lastName = "";
+    private BigDecimal salary;
+    private Date hireDate;
+    private String jobID;
+    private String email;
+    private String phoneNumber;
+}
